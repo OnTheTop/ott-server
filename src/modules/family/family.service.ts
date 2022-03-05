14edb6ead60c1abe308c40026d5bf510666940ff
god9599 <59276134+god9599@users.noreply.github.com>
2022-03-05 22:49:29 +0900

    Refactor: try catch 적용

--- src/modules/family/family.service.ts
+++ src/modules/family/family.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Long, Repository } from 'typeorm';
 import { Family } from '../../entities/family.entity';
@@ -16,54 +16,74 @@ export class FamilyService {
 
   // 회원가입
   async signUp(createFamilyDto: CreateFamilyDto) {
-    const family = await this.saveFamily(createFamilyDto.familyNickname);
-    await this.saveMember(createFamilyDto.memberNickname, family.id);
-    return {
-      familyId: family.id,
-      familyCode: family.familyCode,
-    };
+    try {
+      const family = await this.saveFamily(createFamilyDto.familyNickname);
+      await this.saveMember(createFamilyDto.memberNickname, family.id);
+      return {
+        familyId: family.id,
+        familyCode: family.familyCode,
+      };
+    } catch (error) {
+      throw new InternalServerErrorException(error.message, error);
+    }
   }
 
   // 가족 저장
   async saveFamily(familyNickname: string) {
-    const family = new Family();
-    const code = this.makeRandomString();
-    family.familyNickname = familyNickname;
-    family.familyCode = code;
-    await this.familyRepository.save(family);
+    try {
+      const family = new Family();
+      const code = this.makeRandomString();
+      family.familyNickname = familyNickname;
+      family.familyCode = code;
+      await this.familyRepository.save(family);
 
-    return family;
+      return family;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message, error);
+    }
   }
 
   // 멤버 저장 (방장 설정)
   async saveMember(memberNickname: string, familyId: Long) {
-    const family = await this.familyRepository.findOne({ id: familyId });
-    family.memberCount = family.memberCount + 1;
-    await this.familyRepository.save(family);
+    try {
+      const family = await this.familyRepository.findOne({ id: familyId });
+      family.memberCount = family.memberCount + 1;
+      await this.familyRepository.save(family);
 
-    const member = new Member();
-    member.familiy = family;
-    member.memberNickname = memberNickname;
-    if (family.memberCount == 0) {
-      member.isLeader = true;
+      const member = new Member();
+      member.familiy = family;
+      member.memberNickname = memberNickname;
+      if (family.memberCount == 0) {
+        member.isLeader = true;
+      }
+      await this.memberRepository.save(member);
+
+      return member;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message, error);
     }
-    await this.memberRepository.save(member);
-
-    return member;
   }
 
   // 기존에 있는 가족에 멤버 추가
   async addMember(memberNickname: string, familyCode: string) {
-    const familyId = await this.findFamilyIdByCode(familyCode);
-    return await this.saveMember(memberNickname, familyId);
+    try {
+      const familyId = await this.findFamilyIdByCode(familyCode);
+      return await this.saveMember(memberNickname, familyId);
+    } catch (error) {
+      throw new InternalServerErrorException(error.message, error);
+    }
   }
 
   // 가족 아이디 리턴
   async findFamilyIdByCode(familyCode: string) {
-    const family = await this.familyRepository.findOne({
-      familyCode: familyCode,
-    });
-    return family.id;
+    try {
+      const family = await this.familyRepository.findOne({
+        familyCode: familyCode,
+      });
+      return family.id;
+    } catch (error) {
+      throw new InternalServerErrorException(error.message, error);
+    }
   }
 
   // 가족 코드 생성
